bb8e6b9974b9f0439723477f2fdc93998d9ceff9
gitaccountpl <107807180+gitaccountpl@users.noreply.github.com>
2022-06-25 16:38:21 +0200

    Update resources#views#posts#index.blade.php

--- resources#views#posts#index.blade.php
+++ resources#views#posts#index.blade.php
@@ -1,1 +1,5 @@
-add
+@extends('layouts.php'); <-------- resources/views/layouts.php
+
+@section('content')
+     Index <---------------- tutaj kod (strona do wyswietlenia) ktora ma zawierac informacje z app.blad.php
+@endsection
