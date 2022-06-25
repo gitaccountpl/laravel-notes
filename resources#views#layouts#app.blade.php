a34b2b6f80c50299674f4ef965a4e243596b06a5
gitaccountpl <107807180+gitaccountpl@users.noreply.github.com>
2022-06-25 17:02:18 +0200

    Update resources#views#layouts#app.blade.php

--- resources#views#layouts#app.blade.php
+++ resources#views#layouts#app.blade.php
@@ -6,6 +6,14 @@
       </head>
 
      <body>
+     <nav>
+     Home   \
+     ...     | w tym pliku mozna umiescic elemeny spojne dla wszystkich inny plikow np: index.blade.php
+     ...    /
+     <nav>
+     
+     
+     
   @yield('content')
      </body>
 </html>
